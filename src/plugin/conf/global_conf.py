6499d23f9b2e4627bff3ead66a3c92269631208e
Sooyoung98 <sooyoungkim@mz.co.kr>
2024-01-10 16:56:40 +0900

    feat: added global conf to see if masking works for secret data

---
+++ src/plugin/conf/global_conf.py
@@ -0,0 +1,16 @@
+CONNECTORS = {}
+
+LOG = {
+    "filters": {
+        "masking": {
+            "rules": {
+                "Protocol.verify": ["secret_data"],
+                "Notification.dispatch": ["secret_data"],
+            }
+        }
+    }
+}
+
+HANDLERS = {}
+
+ENDPOINTS = {}
